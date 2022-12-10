96dc3ee6dc23fea7c0273bdf7591f95eebbd15f9
stonkol <hector96fi@yahoo.es>
2022-12-10 16:55:42 +0100

    v1.0 Initialize Arduino and Processing code

---
+++  Gyroscope data <- Arduino/ Gyroscope data <- Arduino.ino
@@ -0,0 +1,47 @@
+// 7.21 Gyroscope ping pong on local files
+/* https://docs.arduino.cc/tutorials/nano-33-ble-sense/imu-gyroscope
+
+1. Accelerometer range is set at [-4, +4]g -/+0.122 mg.
+      -> output data rate is fixed at 104 Hz.
+2. Gyroscope range is set at [-2000, +2000] dps +/-70 mdps.
+      -> output data rate is fixed at 104 Hz. */
+      
+
+
+//// My expanded official EXAMPLE simple gyroscope ////////
+#include <Arduino_LSM9DS1.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  // Serial.print("Gyroscope sample rate = "); Serial.print(IMU.gyroscopeSampleRate()); Serial.println(" Hz");
+  // Serial.println(); 
+  // Serial.println("Gyroscope in degrees/second"); Serial.println("X\tY\tZ");
+}
+
+void loop() {
+  float x, y, z;
+
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(x, y, z);
+    int intX = (int)x; 
+    int intY = (int)y;
+    int intZ = (int)z;  // make the IMU data entero
+
+    // Print x, y axys in INT
+    Serial.print(intX); 
+    Serial.print("\t");
+    Serial.print(intY); 
+    Serial.print('\n');
+
+    // Print x, y axys in FLOAT
+    // Serial.print(x); Serial.print('\t'); Serial.print(y); Serial.print('\t'); Serial.println(z);
+  }
+  delay(111);
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # PingPong-with-Arduino-and-Processing
- 
+
+## TO-DO
+
+- [ ] Retrive data from Arduino
+- [ ] Transfer values to numbers in Processing
+- [ ] a functional x-axis
+- [ ] a functional y-axis
+
+## Maybe TO-DO
+
+### Sound
+
+- [ ] bg sound
+- [ ] hit sound
+- [ ] menu sound
+
+### Screens
+
+- [ ] Theme
+
+- [ ] Start Menu
+
+- [ ] in game options menu
+
+### TECH
+
+- [ ] BLE connection
+
+- [ ] MultiArduino player
